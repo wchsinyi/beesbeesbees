0021339544ff25ae2b03fab12dbf8c11e7efccef
uma <hsinyi.wc.uma@gmail.com>
2019-04-15 11:23:49 -0700

    Draft grub and bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
-var Bee = function() {
+var Bee = function(age, color, job) {
+  // Grub.call(this, food, eat); //get some props set up by Nut
+  // this.age = 5;
+  // this.color = 'yellow';
+  // this.job = 'keep on growing'; 
 };
-
+// Bee.prototype = Object.create(Grub.prototype); //set inheritance
+// Bee.prototype.constructor = Bee; //set Almond constructor func
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,8 @@
 var Grub = function() {
+	this.age= 0;
+	this.color = 'pink';
+	this.color = 'pink';
+	this.food = 'jelly';
+	this.eat = 'function(){};
 };
 
